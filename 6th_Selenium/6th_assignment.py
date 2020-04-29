1f22c37c87edf90262120c46882b99a3e4fa52ff
ichwoni <codnjs0397@naver.com>
2020-04-29 23:57:29 +0900

    6th

--- 6th_Selenium/6th_assignment.py
+++ 6th_Selenium/6th_assignment.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import os
 import time
 
-#path = os.getcwd() + "./chromedriver.exe" #실제로 할 때는 크롬드라이버가 가장 처음에 다운로드됐던 경로로 설정해야 실행됨 
+#path = os.getcwd() + "./chromedriver.exe" #실제로 할 때는 크롬 드라이버가 가장 처음에 다운로드됐던 경로로 설정해야 실행됨 
 
 driver = webdriver.Chrome("C:\chromedriver_win32\chromedriver.exe")
 
